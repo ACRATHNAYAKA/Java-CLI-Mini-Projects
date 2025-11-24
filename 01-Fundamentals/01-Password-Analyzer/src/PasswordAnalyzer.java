1df077bfa76e403b4d27034a588d65df9cef9cd9
ACRATHNAYAKA <avishkachamara71@gmail.com>
2025-11-24 22:08:52 +0530

    update PasswordAnalyzer.java

--- 01-Fundamentals/01-Password-Analyzer/src/PasswordAnalyzer.java
+++ 01-Fundamentals/01-Password-Analyzer/src/PasswordAnalyzer.java
@@ -1,2 +1,271 @@
+import java.io.Console;
+import java.util.Scanner;
+
+
 public class PasswordAnalyzer {
+
+    public static String password = "";
+
+    public static boolean hasLowerCase = false;
+    public static boolean hasUpperCase = false;
+    public static boolean hasNumbers = false;
+    public static boolean hasSymbol = false;
+    public static boolean hasSpace = false;
+
+
+    static String[] MENU_ITEMS = {"[1] Check Password Strength","[2] Generate Strong Password","[3] Security Tips","[4] Exit"};
+
+
+    public static void main(String[] args) {
+        boolean appRunning = true;
+
+        while (appRunning) {
+            clearScrean();
+            drawHeader();
+            drawMenu();
+            performAction(getChoice());
+        }
+    }
+
+    static void drawHeader() {
+        System.out.println("=====================================================");
+        System.out.println("           SMART PASSWORD STRENGTH ANALYZER          ");
+        System.out.println("=====================================================");
+    }
+
+    private static void clearScrean(){
+        System.out.print("\033[H\033[2J");
+        System.out.flush();
+    }
+
+    static void drawMenu(){
+        for (int i = 0; i<MENU_ITEMS.length; i++){
+            System.out.println(MENU_ITEMS[i]);
+        }
+    }
+
+    static int getChoice(){
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter your choice (1-4): ");
+        int choice = Integer.parseInt(scanner.nextLine());
+
+        if(choice<1 || choice>4){
+            System.out.println("Invalid Input");
+            getChoice();
+        }
+        return choice;
+    }
+
+    static boolean performAction(int choice){
+        switch (choice){
+            case 1:
+                checkPasswordStrength();
+                break;
+            case 2:
+                GenerateStrongPassword();
+                break;
+            case 3:
+                securityTips();
+                break;
+            case 4:
+                goodBye();
+                return false;
+        }
+
+        return true;
+    }
+
+
+    static void checkPasswordStrength(){
+        clearScrean();
+        Scanner scanner = new Scanner(System.in);
+        Console console = System.console();
+
+        drawHeader();
+
+        if (console == null){
+            System.out.print("Enter Your Password : ");
+            password = scanner.nextLine();
+        }
+        else {
+            char[] cdmPassword = console.readPassword("Enter Your Password : ");
+            password = new String(cdmPassword);
+        }
+        System.out.println("Analyse Your Password.......");
+        System.out.println();
+        clearScrean();
+
+        System.out.println("=====================================================");
+        System.out.println("                         RESULT                      ");
+        System.out.println("-----------------------------------------------------");
+
+        System.out.println("Your Password : "+password);
+
+        passwordScore();
+        estimatedCrackTime();
+        checkPassed();
+    }
+
+    public static void estimatedCrackTime(){
+
+        String estimateCrackTime = "";
+        int poolSize = 0;
+
+        if (hasLowerCase){
+            poolSize+=26;
+        }
+        if (hasUpperCase){
+            poolSize+=26;
+        }
+        if(hasNumbers){
+            poolSize+=10;
+        }
+        if (hasSymbol){
+            poolSize+=33;
+        }
+
+        double combinations = Math.pow(poolSize,password.length());
+        double guessesPerSecond = 1000_000_000.0;
+        double seconds = combinations/guessesPerSecond;
+
+        double minute = 60;
+        double hour = minute*60;
+        double day = hour*24;
+        double years = day*365;
+
+        if(seconds<1){
+            estimateCrackTime = "Instantly";
+        }
+        else if (seconds<minute) {
+            estimateCrackTime = String.format("%.2f seconds",seconds);
+        }
+        else if(seconds<hour){
+            estimateCrackTime = String.format("%.2f minutes",seconds/minute);
+        }
+        else if (seconds<day) {
+            estimateCrackTime = String.format("%.2f hours",seconds/hour);
+        }
+        else if (seconds<years){
+            estimateCrackTime = String.format("%.2f days",seconds/day);
+        }
+        else if (seconds/years >1000_000) {
+            estimateCrackTime = "Centuries";
+        }
+        else {
+            estimateCrackTime = String.format("%.2f years", seconds / years);
+        }
+
+        System.out.println("Estimated Crack Time : "+estimateCrackTime);
+    }
+
+    public static void passwordScore(){
+
+        int score = 0;
+        String status = "";
+
+        int passwordLength;
+
+
+        passwordLength = password.length();
+        int lengthScore = passwordLength*4;
+        if(lengthScore>40){
+            lengthScore = 40;
+        }
+        score+=lengthScore;
+
+        for (char checkItem : password.toCharArray()){
+            if (Character.isDigit(checkItem)){
+                hasNumbers = true;
+            }
+
+            else if (Character.isUpperCase(checkItem)){
+                hasUpperCase = true;
+            }
+
+            else if(Character.isLowerCase(checkItem)){
+                hasLowerCase = true;
+            }
+
+            else if (Character.isWhitespace(checkItem)){
+                hasSpace = true;
+            }
+            else {
+                hasSymbol =true;
+            }
+        }
+
+        if(hasLowerCase) {
+            score += 10;
+        }
+        if(hasUpperCase) {
+            score += 10;
+        }
+        if (hasNumbers){
+            score += 15;
+        }
+        if(hasSymbol) {
+            score += 15;
+        }
+
+        if(hasLowerCase && hasUpperCase && hasNumbers && hasSymbol && lengthScore>=32){
+            score +=10;
+        }
+
+        if (score<=70){
+            status = "Week";
+        }
+        else if (score<=80){
+            status = "Medium";
+        }
+        else if(score<=85){
+            status = "Strong";
+        }
+        else if (score<=100) {
+            status = "Very Strong";
+        }
+
+        System.out.println();
+        System.out.println("Strength Score : "+score+"%");
+        System.out.println("Validate : "+status);
+
+
+    }
+
+    public static void checkPassed(){
+        System.out.println();
+        System.out.println("Check Passed:");
+        System.out.println("Length "+password.length()+" Characters");
+        if (hasLowerCase){
+            System.out.println("✓ Contains Lowercase Letters");
+        }
+        if (hasUpperCase){
+            System.out.println("✓ Contains Uppercase Letters");
+        }
+        if (hasNumbers){
+            System.out.println("✓ Contains Numbers");
+        }
+        if(hasSymbol){
+            System.out.println("✓ Contains Special Characters");
+        }
+        System.out.println();
+        System.out.println("Pro Tip : Add 3+ random words → becomes UNBREAKABLE!");
+        System.out.println("Example : I-Drink-BlackMilk-At-10PM");
+        System.out.println();
+
+    }
+
+
+    static void GenerateStrongPassword(){
+
+    }
+
+    static void securityTips(){
+
+    }
+
+    static void goodBye(){
+
+    }
+
 }
