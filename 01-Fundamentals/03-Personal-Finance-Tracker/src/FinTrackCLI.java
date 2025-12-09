e8fb1483d1b94d4e38f78b5add1f6b4e3ba80774
ACRATHNAYAKA <avishkachamara71@gmail.com>
2025-12-09 22:12:50 +0530

    feat: create drawHeader method

--- 01-Fundamentals/03-Personal-Finance-Tracker/src/FinTrackCLI.java
+++ 01-Fundamentals/03-Personal-Finance-Tracker/src/FinTrackCLI.java
@@ -2,4 +2,10 @@ public class FinTrackCLI {
     public static void main(String[] args) {
 
     }
+
+    private static void drawHeader(String headerName){
+        System.out.println("==========================================================");
+        System.out.println("                "+headerName+"                            ");
+        System.out.println("==========================================================");
+    }
 }
