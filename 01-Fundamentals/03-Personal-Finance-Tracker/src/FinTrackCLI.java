2e279d605aad53725739715a3a0e57810a9656de
ACRATHNAYAKA <avishkachamara71@gmail.com>
2025-12-11 16:51:04 +0530

    wip: initial dashboard setup

--- 01-Fundamentals/03-Personal-Finance-Tracker/src/FinTrackCLI.java
+++ 01-Fundamentals/03-Personal-Finance-Tracker/src/FinTrackCLI.java
@@ -1,6 +1,38 @@
+import java.time.LocalDate;
+import java.util.Date;
+
 public class FinTrackCLI {
     public static void main(String[] args) {
+        boolean appRunning = true;
 
+        while (appRunning){
+            clearScrean();
+            drawHeader("FinTrack CLI");
+            dashBord("Chamara",10000,1200);
+            appRunning = false;
+        }
+    }
+
+    private static void dashBord(String userName, double balance, double total) {
+        LocalDate date = LocalDate.now();
+        System.out.println("user : " + userName + "         " + "Date : " + date);
+        System.out.println("__________________________________________________________");
+        String balanceStatus = "";
+        double balanceScore = balance / total * 100;
+        if (balanceScore >= 75)
+            balanceStatus = "Healthy";
+        else if (balanceScore >= 40)
+            balanceStatus = "Normal";
+        else
+            balanceStatus = "Week";
+
+        System.out.println("Net Balance : "+balance+" ["+balanceStatus+"] ");
+
+    }
+
+    private static void clearScrean(){
+        System.out.print("\033[H\033[2J");
+        System.out.flush();
     }
 
     private static void drawHeader(String headerName){
